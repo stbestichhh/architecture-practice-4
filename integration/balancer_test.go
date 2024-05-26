d51af2a7bfab565dc794ed7c07f5132e8078bae5
anastasia-sl <anastasiaslynko2206@gmail.com>
2024-05-26 16:05:12 +0300

    implemented integration test for the balancer

--- integration/balancer_test.go
+++ integration/balancer_test.go
@@ -19,12 +19,43 @@ func TestBalancer(t *testing.T) {
 		t.Skip("Integration test is not enabled")
 	}
 
-	// TODO: Реалізуйте інтеграційний тест для балансувальникка.
+	// Send a single request to check basic functionality
 	resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
 	if err != nil {
 		t.Error(err)
 	}
+	defer resp.Body.Close()
 	t.Logf("response from [%s]", resp.Header.Get("lb-from"))
+
+	// Call the helper function to verify load balancing
+	verifyLoadBalancing(t)
+}
+
+func verifyLoadBalancing(t *testing.T) {
+	const numRequests = 10
+	backends := make(map[string]int)
+
+	for i := 0; i < numRequests; i++ {
+		resp, err := client.Get(fmt.Sprintf("%s/api/v1/some-data", baseAddress))
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		defer resp.Body.Close()
+
+		backend := resp.Header.Get("lb-from")
+		if backend == "" {
+			t.Error("Response header 'lb-from' is empty")
+			return
+		}
+		backends[backend]++
+	}
+
+	if len(backends) < 2 {
+		t.Errorf("Load balancer did not distribute requests to multiple backends: %v", backends)
+	} else {
+		t.Logf("Requests were distributed across the following backends: %v", backends)
+	}
 }
 
 func BenchmarkBalancer(b *testing.B) {
